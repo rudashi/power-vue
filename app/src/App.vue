fb433224a646d0ae51a3b7f5df525ac4f6ccc7b1
Rudashi <rudashi@gmail.com>
2026-02-04 08:30:21 +0100

    Add example app for `@power-vue` usage

---
+++ app/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <pre>{{  locale }}</pre>
+</template>
+
+<script setup lang="ts">
+    import { useLocale } from 'power-vue'
+
+    const locale = useLocale()
+</script>
---
+++ app/src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import { createPowerVue } from 'power-vue'
+import { pl } from 'power-vue/locale'
+import App from './App.vue'
+
+createApp(App)
+    .use(createPowerVue({
+        locale: {
+            locale: 'pl',
+            messages: { pl },
+        },
+    }))
+    .mount("#app")
