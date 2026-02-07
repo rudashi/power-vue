968b7779a3c5a3336770d0872ffb059fcb74accd
Rudashi <rudashi@gmail.com>
2026-02-07 22:19:46 +0100

    feat: Add `PowerVueInstance` type

--- packages/powervue/src/framework.ts
+++ packages/powervue/src/framework.ts
@@ -1,21 +1,28 @@
-import { App } from 'vue'
 import { createTheme, ThemeSymbol } from 'power-vue/composables/theme'
 import { createLocale, LocaleSymbol } from 'power-vue/composables/locale'
 import { createIcons, IconSymbol } from 'power-vue/composables/icons'
 
-import type { ThemeOptions } from 'power-vue/composables/theme'
-import type { LocaleOptions } from 'power-vue/composables/locale'
+import type { App, Component, Directive } from 'vue'
+import type { ThemeInstance, ThemeOptions } from 'power-vue/composables/theme'
+import type { LocaleInstance, LocaleOptions } from 'power-vue/composables/locale'
 import type { IconOptions } from 'power-vue/composables/icons'
 
-export interface PowerVueOptions {
+export type PowerVueOptions = {
     theme?: ThemeOptions
     icons?: IconOptions
     locale?: LocaleOptions
-    components?: Record<string, any>
-    directives?: Record<string, any>
+    components?: Record<string, Component>
+    directives?: Record<string, Directive>
 }
 
-export function createPowerVue(options: PowerVueOptions = {}) {
+export type PowerVueInstance = {
+    install: (app: App) => void
+    theme: ThemeInstance
+    icons: IconOptions
+    locale: LocaleInstance
+}
+
+export function createPowerVue(options: PowerVueOptions = {}): PowerVueInstance {
     const {
         components = {},
         directives = {},
